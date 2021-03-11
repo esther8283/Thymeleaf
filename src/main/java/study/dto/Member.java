a34293b7e3760c53ae0ec34d5d2cb614e7701e78
DESKTOP-92CN66O\user <esther8283@naver.com>
2021-03-11 20:19:15 +0900

    Member

---
+++ src/main/java/study/dto/Member.java
@@ -0,0 +1,45 @@
+package study.dto;
+
+public class Member {
+	private String name;
+	private String email;
+	private String tel;
+	private String address;
+	
+	public Member() {
+		this("홍길동", "asdf@gmail.com", "010-1234-5678", "부산");
+	}
+	
+	public Member(String name, String email, String tel, String address) {
+		this.name = name;
+		this.email = email;
+		this.tel = tel;
+		this.address = address;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
